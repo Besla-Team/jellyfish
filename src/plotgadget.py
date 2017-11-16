32acf96a014f39d5515ea2c3ea2190dcf9c79e89
ekta1224 <ektapatel@email.arizona.edu>
2017-11-16 14:32:13 -0700

    scripts to plot gadget output by type and all gadget particles added

---
+++ src/plotgadget.py
@@ -0,0 +1,158 @@
+from pygadgetreader import *
+import numpy as np
+import yt
+from yt.units import parsec, Msun, kpc,g
+from octopus import CM, CM_disk_potential
+import matplotlib.pyplot as plt
+
+class PlotGadget:
+
+    def __init__(self, path, type, box_size=1000):
+        """ Inputs: path to Gadget3 snapshot, particle type, box_size (kpc)
+        """
+        print path
+        self.path = path
+        self.box_size = box_size
+        pos = readsnap(self.path, 'pos',type)
+        vel = readsnap(self.path, 'vel', type)
+        mass = readsnap(self.path, 'mass', type)
+        pot = readsnap(self.path, 'pot', type)
+        print len(pos), len(vel), len(mass), len(pot)
+
+        if type == 'dm' or 'bulge':
+            self.com, self.vcom = CM(pos, vel)
+
+        if type == 'disk':
+            self.com, self.vcom = CM_disk_potential(pos, vel, pot)
+
+        x,y,z = pos[:,0], pos[:,1], pos[:,2]  
+        x = x - self.com[0]
+        y = y - self.com[1]
+        z = z - self.com[2]
+
+        r = (x**2 + y**2 + z**2)**0.5
+
+        cut = np.where(r < box_size)[0]
+
+        self.pos  = pos[cut]
+        self.x, self.y, self.z = self.pos[:,0] - self.com[0], self.pos[:,1]-self.com[1], self.pos[:,2]-self.com[2] 
+        self.r = (self.x**2 + self.y**2 + self.z**2)**0.5
+        self.vel = vel[cut]
+        self.vx, self.vy, self.vz = self.vel[:,0], self.vel[:,1], self.vel[:,2]
+        
+    
+        self.mass = mass[cut]
+        self.pot = pot[cut]
+
+        self.data = {'particle_position_x': self.x,\
+                'particle_position_y': self.y,\
+                'particle_position_z': self.z,\
+                'particle_velocity_x': self.vel[:,0],\
+                'particle_velocity_y': self.vel[:,1],\
+                'particle_velocity_z': self.vel[:,2],\
+                'particle_mass': self.mass,\
+                'particle_pot' : self.pot}
+
+
+        bbox_lim = 2e5 #kpc
+
+        self.bbox = [[-bbox_lim,bbox_lim],[-bbox_lim,bbox_lim],[-bbox_lim,bbox_lim]]
+
+    def totalmass_bytype(self):
+        return np.sum(self.data['particle_mass'])*1e10
+
+    def header(self):
+        return readheader(self.path, 'header')
+
+    def header_time(self):
+        return readheader(self.path, 'time')
+
+    def peak_density_loc(self):
+        ds = yt.load(self.path,bounding_box=self.bbox)
+        ad= ds.all_data()
+        density = ad[("deposit","all_density")]
+        wdens = np.where(density == np.max(density))
+        coordinates = ad[("all","Coordinates")]
+        center = coordinates[wdens][0]
+        return center 
+        
+    def plot_fullbox_projected_density(self, projection_plane, savename):
+        ds = yt.load(self.path,bounding_box=self.bbox)
+        ad= ds.all_data()
+        px = yt.ProjectionPlot(ds, projection_plane, ('deposit', 'all_density'))
+        px.annotate_text((0.7, 0.85), '%s Gyr'%round(self.header_time(),2), coord_system='figure', text_args={'color':'white'})
+        px.save('%s'%savename)
+        return
+        
+    def plot_projected_density(self, projection_plane, savename, zoom=False):
+        center = self.com
+        ds = yt.load(self.path,bounding_box=self.bbox)
+        ad = ds.all_data()
+        px = yt.ProjectionPlot(ds, projection_plane, ('deposit', 'all_density'), center=center, width=self.box_size)
+        if zoom:
+            px.zoom(zoom)
+        
+        px.annotate_text((0.7, 0.85), '%s Gyr'%round(self.header_time(),2), coord_system='figure', text_args={'color':'white'})
+        px.save('%s'%savename)
+        
+        return
+
+    def plot_all_density(self, projection_plane, savename):
+        '''CAUTION: this plots the DISK density
+        '''
+        center = self.com
+        ds = yt.load(self.path,bounding_box=self.bbox)
+        ad = ds.all_data()
+
+        slc = yt.SlicePlot(ds, projection_plane, ('deposit', 'Disk_density'), center=center)
+        slc.set_width((self.box_size, 'kpc'))
+        slc.annotate_text((0.7, 0.85), '%s Gyr'%round(self.header_time(),2), coord_system='figure', text_args={'color':'white'})
+        slc.save('%s'%savename)
+        return
+
+    def plot_particle_density(self, projection_plane, savename):
+        bbox = 1.1*np.array([[np.min(self.x), np.max(self.x)], [np.min(self.y), np.max(self.y)], [np.min(self.z), np.max(self.z)]])
+        ds = yt.load_particles(self.data, length_unit=kpc,mass_unit=1e10,  bbox = bbox, n_ref=4)
+        center = self.com
+        p = yt.ParticleProjectionPlot(ds, projection_plane, ['particle_mass'])#, center=center)#, width=self.box_size, depth=self.box_size)
+        p.set_colorbar_label('particle_mass', 'Msun')
+        #p.zoom(2)
+        p.annotate_text((0.7, 0.85), '%s Gyr'%round(self.header_time(),2), coord_system='figure', text_args={'color':'white'})
+        p.save('%s'%savename)
+        return
+
+    def plot_rot_curve(self, bin_width, savename=False, newfig=False, xlim=20):
+        rs = np.arange(1., self.box_size, bin_width)
+        vcs = []
+        G = 4.498768e-6
+        for r in rs:
+            cut = np.where(self.r < r)[0]
+            mass = np.sum(self.mass[cut])
+            vc = np.sqrt(G*mass*1e10/r)
+            vcs.append(vc)
+
+        if newfig:
+            plt.figure()
+
+        plt.plot(rs, vcs, lw=1.5, label='%s Gyr'%round(self.header_time(),2))
+        plt.xlim(0.,xlim)
+        plt.xlabel(r'$\rm galactocentric \; radius \; [kpc]$', fontsize=16)
+        plt.ylabel(r'$\rm v_{circ}\; [km \; s^{-1}]$', fontsize=16)
+        plt.legend(loc='best', ncol=2,fontsize=12)
+        if savename:
+            plt.savefig('%s'%savename)
+        return
+            
+if __name__ == "__main__":
+    path = './m31a_25oct_gadget3_m31a_25oct_000'
+    this = PlotGadget(path, 350)
+    print this.plot_enclosed_mass('test3b_m31_enclosed_mass.pdf')
+
+#     print this.header_time()
+#     print this.totalmass()
+#     print this.find_bbox_lim()
+#     print this.peak_density_loc()
+#     this.plot_fullbox_projected_density(0,'test3b_m31_fullbox')
+#     this.plot_projected_density(0, 'test3b_m31')
+#     this.plot_all_density(0, 'test3b_m31')
+#     this.plot_particle_density(0, 'test3b_m31')
---
+++ src/plotgadgetall.py
@@ -0,0 +1,120 @@
+from pygadgetreader import *
+import numpy as np
+import yt
+from yt.units import parsec, Msun, kpc,g
+from octopus import CM, CM_disk_potential
+import matplotlib.pyplot as plt
+
+class PlotGadgetAll:
+
+    def __init__(self, path, box_size=1000):
+        """ Inputs: path to Gadget3 snapshot, box_size (kpc)
+        """
+        print path
+        self.path = path
+        self.box_size = box_size
+
+        self.dm = readsnap(self.path, 'mass', 'dm')
+        self.disk = readsnap(self.path, 'mass', 'disk')
+        self.bulge = readsnap(self.path, 'mass', 'bulge')
+
+
+        #print len(dm), np.min(dm), np.max(dm)
+        #print len(disk), np.min(disk), np.max(disk)
+        #print len(bulge), np.min(bulge), np.max(bulge)
+
+        self.dmpos = readsnap(self.path, 'pos','dm')
+        self.dmvel = readsnap(self.path, 'vel', 'dm')
+        self.diskpos = readsnap(self.path, 'pos','disk')
+        self.diskvel = readsnap(self.path, 'vel', 'disk')
+        self.diskpot = readsnap(self.path, 'pot', 'disk')
+        self.bulgepos = readsnap(self.path, 'pos','bulge')
+
+        self.halo_com, self.halo_vcom = CM(self.dmpos, self.dmvel)
+        print self.halo_com
+        self.disk_com, self.disk_vcom = CM_disk_potential(self.diskpos, self.diskvel, self.diskpot)
+        print self.disk_com
+
+
+    def header_time(self):
+        return readheader(self.path, 'time')
+
+    def plot_enclosed_mass(self, savename=False, newfig=False):
+        ''' This function assumes that there is a DM, disk, and bulge component. 
+        '''
+
+        rs = np.arange(1., self.box_size, 1.)
+
+        masses = []
+        for rmin in rs:
+            thismass = 0.
+            for pos,mass in zip([self.dmpos, self.diskpos, self.bulgepos], [self.dm, self.disk, self.bulge]):
+                x,y,z = pos[:,0], pos[:,1], pos[:,2]  
+                x = x - self.halo_com[0]
+                y = y - self.halo_com[1]
+                z = z - self.halo_com[2]
+
+                r = (x**2 + y**2 + z**2)**0.5
+                cut = np.where(r < rmin)[0]
+                thismass += np.sum(mass[cut])
+            if rmin ==5:
+                print thismass
+            masses.append(thismass*1e10)
+
+        if newfig:
+            plt.figure()
+        plt.plot(rs, masses, lw=1.5, label='%s Gyr'%round(self.header_time(),2))
+        plt.xlabel('distance from COM')
+        plt.ylabel('total mass enclosed')
+        plt.yscale("log")
+        plt.xscale("log")
+        plt.legend(loc='best', ncol=2, fontsize=12)
+        if savename:
+            plt.savefig('%s'%savename)
+        return
+
+
+    def plot_total_rot_curve(self, bin_width,savename=False, newfig=False, xlim=20):
+        ''' Currently all components use the halo COM, but really the disk should use the disk COM, etc.
+        '''
+
+        rs = np.arange(1., self.box_size, bin_width)
+        vcs = []
+        G = 4.498768e-6
+
+        for rmin in rs:
+            print rmin
+            thismass = 0.
+            for pos,mass in zip([self.dmpos, self.diskpos, self.bulgepos], [self.dm, self.disk, self.bulge]):
+                x,y,z = pos[:,0], pos[:,1], pos[:,2]  
+                x = x - self.halo_com[0]
+                y = y - self.halo_com[1]
+                z = z - self.halo_com[2]
+                r = (x**2 + y**2 + z**2)**0.5
+                cut = np.where(r < rmin)[0]
+                thismass += np.sum(mass[cut])
+            vc = np.sqrt(G*thismass*1e10/rmin)
+            vcs.append(vc)                
+
+        if newfig:
+            plt.figure()
+
+        plt.plot(rs, vcs, lw=1.5, label=r'%s Gyr, $\rm v_{max}=%s$'%(round(self.header_time(),2), round(np.max(vcs),2)))
+        plt.xlim(0.,xlim)
+        plt.xlabel(r'$\rm galactocentric \; radius \; [kpc]$', fontsize=16)
+        plt.ylabel(r'$\rm v_{circ}\; [km \; s^{-1}]$', fontsize=16)
+        plt.legend(loc='best', ncol=2, fontsize=12)
+
+        if savename:
+            print 'saving figure'
+            plt.savefig('%s'%savename)
+            
+        return
+
+if __name__ == "__main__":
+    path = './m31a_25oct/gadget3_m31a_25oct_000'
+    this = PlotGadgetAll(path, 350)
+    
+    this.plot_total_rot_curve(savename='test_PlotGadgetAll_rotcurve.pdf', newfig=True)
+    this.plot_enclosed_mass(savename='test_PlotGadgetAll.pdf', newfig=True)
+    
