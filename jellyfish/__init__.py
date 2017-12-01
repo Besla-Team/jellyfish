8676032102dcd63987f73e89878293098c946c16
jngaravitoc <jngaravitoc@email.arizona.edu>
2017-12-01 15:03:28 -0700

    working init.py

---
+++ jellyfish/__init__.py
@@ -0,0 +1,9 @@
+#usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .com import *
+from .orbit import *
+from .shapes import *
+from .physical_properties import *
+from .plotgadget import *
+from .plotgadgetall import *
