8ae38e0fab0b6055bc3302f27a897b85dd339c0c
jngaravitoc <jngaravitoc@email.arizona.edu>
2017-12-01 14:40:55 -0700

    src to jellyfihs

---
+++ jellyfish/com.py
@@ -0,0 +1,143 @@
+#!/sr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import sys
+from pygadgetreader import *
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+#Function that computes the center of mass for the halo and disk and
+# the corresponsing orbits for the host and satellite simultaneously
+
+def re_center(vec, cm):
+    """
+    Re center a halo to its center of mass.
+    """
+
+    pos[:,0] -= cm[0]
+    pos[:,1] -= cm[1]
+    pos[:,2] -= cm[2]
+
+    return pos
+
+
+def host_sat_particles(xyz, vxyz, pids, Nhost_particles):
+    """
+    Return the host and the sat particles
+    positions and velocities.
+
+    Parameters:
+    -----------
+    xyz: snapshot coordinates with shape (n,3)
+    vxys: snapshot velocities with shape (n,3)
+    pids: particles ids
+    Nhost_particles: Number of host particles in the snapshot
+    Returns:
+    --------
+    xyz_mw, vxyz_mw, xyzlmc, vxyz_lmc: coordinates and velocities of
+    the host and the sat.
+
+    """
+    sort_indexes = np.sort(pids)
+    N_cut = sort_indexes[Nhost_particles]
+    host_ids = np.where(pids<N_cut)[0]
+    sat_ids = np.where(pids>=N_cut)[0]
+
+    return xyz[host_ids], vxyz[host_ids], xyz[sat_ids], vxyz[sat_ids]
+
+
+def com_disk_potential(xyz, vxyz, Pdisk): 
+    V_radius = 2
+    vx = vxyz[:,0]
+    vy = vxyz[:,1]
+    vz = vxyz[:,2]
+    x = xyz[:,0]
+    y = xyz[:,1]
+    z = xyz[:,2]
+
+    min_pot = np.where(Pdisk==min(Pdisk))[0]
+    x_min = x[min_pot]
+    y_min = y[min_pot]
+    z_min = z[min_pot]
+    # This >2.0 corresponds to the radius in kpc of the particles that
+    # I am taking into account to compute the CM
+    avg_particles = np.where(np.sqrt((x-x_min)**2.0 + (y-y_min)**2.0 + (z-z_min)**2.0)<V_radius)[0]
+    x_cm = sum(x[avg_particles])/len(avg_particles)
+    y_cm = sum(y[avg_particles])/len(avg_particles)
+    z_cm = sum(z[avg_particles])/len(avg_particles)
+    vx_cm = sum(vx[avg_particles])/len(avg_particles)
+    vy_cm = sum(vy[avg_particles])/len(avg_particles)
+    vz_cm = sum(vz[avg_particles])/len(avg_particles)
+    return np.array([x_cm, y_cm, z_cm]), np.array([vx_cm, vy_cm, vz_cm])
+
+def velocities_com(cm_pos, pos, vel, rvir):
+    """
+    Function to compute the COM velocity in a sphere of 10% of Rvir (kpc)
+    """
+    # Compute the distance with respect to the COM
+    R_cm = ((pos[:,0]-cm_pos[0])**2 + (pos[:,1]-cm_pos[1])**2 + (pos[:,2]-cm_pos[2])**2)**0.5
+    # Select the particles inside 15 kpc
+    index = np.where(R_cm < 0.1*rvir)[0]
+    # Compute the velocities of the COM:
+    velx_cm = sum(vel[index,0])/len(vel[index,0])
+    vely_cm = sum(vel[index,1])/len(vel[index,1])
+    velz_cm = sum(vel[index,2])/len(vel[index,2])
+
+    return velx_cm, vely_cm, velz_cm
+
+
+def CM(xyz, vxyz, delta=0.025):
+    """
+    Compute the center of mass coordinates and velocities of a halo
+    using the Shrinking Sphere Method Power et al 2003.
+    It iterates in radii until reach a convergence given by delta
+    or 1% of the total number of particles.
+
+    Parameters:
+    -----------
+    xyz: cartesian coordinates with shape (n,3)
+    vxys: cartesian velocities with shape (n,3)
+    delta(optional): Precision of the CM, D=0.025
+
+    Returns:
+    --------
+    rcm, vcm: 2 arrays containing the coordinates and velocities of
+    the center of mass with reference to a (0,0,0) point.
+
+    """
+    N_i = len(xyz)
+    N = N_i
+
+    xCM = 0.0
+    yCM = 0.0
+    zCM = 0.0
+
+    xCM_new = sum(xyz[:,0])/N_i
+    yCM_new = sum(xyz[:,1])/N_i
+    zCM_new = sum(xyz[:,2])/N_i
+
+
+    while (((np.sqrt((xCM_new-xCM)**2 + (yCM_new-yCM)**2 + (zCM_new-zCM)**2) > delta) & (N>N_i*0.01)) | (N>1000)):
+        xCM = xCM_new
+        yCM = yCM_new
+        zCM = zCM_new
+        # Re-centering sphere
+        xyz[:,0] = xyz[:,0]
+        xyz[:,1] = xyz[:,1]
+        xyz[:,2] = xyz[:,2]
+        R = np.sqrt((xyz[:,0]-xCM_new)**2 + (xyz[:,1]-yCM_new)**2 + (xyz[:,2]-zCM_new)**2)
+        Rmax = np.max(R)
+        # Reducing Sphere by its 2.5%
+        index = np.where(R<Rmax*0.975)[0]
+        xyz = xyz[index]
+        N = len(xyz)
+        #Computing new CM coordinates and velocities
+        xCM_new = np.sum(xyz[:,0])/N
+        yCM_new = np.sum(xyz[:,1])/N
+        zCM_new = np.sum(xyz[:,2])/N
+
+    vxCM_new, vyCM_new, vzCM_new = velocities_com([xCM_new, yCM_new, zCM_new], xyz, vxyz)
+    return np.array([xCM_new, yCM_new, zCM_new]), np.array([vxCM_new, vyCM_new, vzCM_new])
+
---
+++ jellyfish/plotgadgetall.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, absolute_import
+
+from pygadgetreader import *
+import numpy as np
+import yt
+from yt.units import parsec, Msun, kpc,g
+from octopus import CM, CM_disk_potential
+import matplotlib.pyplot as plt
+
+class PlotGadgetAll:
+
+    def __init__(self, path, box_size=1000):
+        """ Inputs: path to Gadget3 snapshot, box_size (kpc)
+        """
+        print path
+        self.path = path
+        self.box_size = box_size
+
+        self.dmmass = readsnap(self.path, 'mass', 'dm')
+        self.diskmass = readsnap(self.path, 'mass', 'disk')
+        self.bulgemass = readsnap(self.path, 'mass', 'bulge')
+
+
+        #print len(dm), np.min(dm), np.max(dm)
+        #print len(disk), np.min(disk), np.max(disk)
+        #print len(bulge), np.min(bulge), np.max(bulge)
+
+        self.dmpos = readsnap(self.path, 'pos','dm')
+        self.dmvel = readsnap(self.path, 'vel', 'dm')
+        self.diskpos = readsnap(self.path, 'pos','disk')
+        self.diskvel = readsnap(self.path, 'vel', 'disk')
+        self.diskpot = readsnap(self.path, 'pot', 'disk')
+        self.bulgepos = readsnap(self.path, 'pos','bulge')
+
+        self.halo_com, self.halo_vcom = CM(self.dmpos, self.dmvel)
+        print self.halo_com
+        self.disk_com, self.disk_vcom = CM_disk_potential(self.diskpos, self.diskvel, self.diskpot)
+        print self.disk_com
+
+
+    def header_time(self):
+        return readheader(self.path, 'time')
+
+    def plot_enclosed_mass(self, savename=False, newfig=False):
+        ''' This function assumes that there is a DM, disk, and bulge component. 
+        '''
+
+        rs = np.arange(1., self.box_size, 1.)
+
+        masses = []
+        for rmin in rs:
+            thismass = 0.
+            for pos,mass in zip([self.dmpos, self.diskpos, self.bulgepos], [self.dmmass, self.diskmass, self.bulgemass]):
+                x,y,z = pos[:,0], pos[:,1], pos[:,2]
+                x = x - self.halo_com[0]
+                y = y - self.halo_com[1]
+                z = z - self.halo_com[2]
+
+                r = (x**2 + y**2 + z**2)**0.5
+                cut = np.where(r < rmin)[0]
+                thismass += np.sum(mass[cut])
+            if rmin ==5:
+                print thismass
+            masses.append(thismass*1e10)
+
+        if newfig:
+            plt.figure()
+        plt.plot(rs, masses, lw=1.5, label='%s Gyr'%round(self.header_time(),2))
+        plt.xlabel('distance from COM')
+        plt.ylabel('total mass enclosed')
+        plt.yscale("log")
+        plt.xscale("log")
+        plt.legend(loc='best', ncol=2, fontsize=12)
+        if savename:
+            plt.savefig('%s'%savename)
+        return
+
+
+    def plot_total_rot_curve(self, bin_width, savename=False, newfig=False, xlim=20):
+        ''' Currently all components use the halo COM, but really the disk should use the disk COM, etc.
+        '''
+
+        rs = np.arange(1., self.box_size, bin_width)
+        vcs = []
+        G = 4.498768e-6
+
+        for rmin in rs:
+            print rmin
+            thismass = 0.
+            for pos,mass in zip([self.dmpos, self.diskpos, self.bulgepos], [self.dmmass, self.diskmass, self.bulgemass]):
+                x,y,z = pos[:,0], pos[:,1], pos[:,2]
+                x = x - self.halo_com[0]
+                y = y - self.halo_com[1]
+                z = z - self.halo_com[2]
+                r = (x**2 + y**2 + z**2)**0.5
+                cut = np.where(r < rmin)[0]
+                thismass += np.sum(mass[cut])
+            vc = np.sqrt(G*thismass*1e10/rmin)
+            vcs.append(vc)
+
+        if newfig:
+            plt.figure()
+
+        plt.plot(rs, vcs, lw=1.5, label=r'%s Gyr, $\rm v_{max}=%s$'%(round(self.header_time(),2), round(np.max(vcs),2)))
+        plt.xlim(0.,xlim)
+        plt.xlabel(r'$\rm galactocentric \; radius \; [kpc]$', fontsize=16)
+        plt.ylabel(r'$\rm v_{circ}\; [km \; s^{-1}]$', fontsize=16)
+        plt.legend(loc='best', ncol=2, fontsize=12)
+
+        if savename:
+            print 'saving figure'
+            plt.savefig('%s'%savename)
+
+        return
+
+    def rho_enclosed(self, rmin=0, rmax=300, nbins=30):
+        """
+        Compute the density profile of a halo with its componenets.
+
+        Input:
+        ------
+        rmin : float
+            Minimum radius to compute the density profile (default=0).
+
+        rmax : float
+            Maximum radius to compute the density profile (default=300)
+
+        nbins : int
+            Number of radial bins to compute the density profile.
+
+        Output:
+        ------
+        r : numpy 1D array.
+            Array with the radial bins.
+        rho : numpy array.
+            Array with the density in each radial bin.
+
+        """
+
+        assert type(nbins) == int, "nbins should be an int variable."
+
+        r_posh = np.sqrt(self.dmpos[:,0]**2 + self.dmpos[:,1]**2 + self.dmpos[:,2]**2)
+        r_posd = np.sqrt(self.diskpos[:,0]**2 + self.diskpos[:,1]**2 + self.diskpos[:,2]**2)
+        r_posb = np.sqrt(self.bulgepos[:,0]**2 + self.bulgepos[:,1]**2 + self.diskpos[:,2]**2)
+
+        r = np.linspace(rmin, rmax, nbins-1)
+
+        rho = np.zeros(nbins-1)
+
+        # Loop over the radial bins.
+        for i in range(1, len(r)):
+            indexh = np.where((r_posh<r[i]) & (r_posh>r[i-1]))[0]
+            indexd = np.where((r_posd<r[i]) & (r_posd>r[i-1]))[0]
+            indexb = np.where((r_posb<r[i]) & (r_posb>r[i-1]))[0]
+
+            rho[i-1] = (3*(len(indexh)*self.dmmass[0]\
+                           + len(indexd)*self.diskmass[0]\
+                           + len(indexb)*self.bulgemass[0])) / (4*np.pi*r[i]**3)
+
+        return r, rho
+
+if __name__ == "__main__":
+#     path = './m31a_25oct/gadget3_m31a_25oct_000'
+#     this = PlotGadgetAll(path, 350)
+    
+#     this.plot_total_rot_curve(savename='test_PlotGadgetAll_rotcurve.pdf', newfig=True)
+#     this.plot_enclosed_mass(savename='test_PlotGadgetAll.pdf', newfig=True)
+    
